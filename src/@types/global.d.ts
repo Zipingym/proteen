52f95ec066c47aa472e23d51f7f6a087c99b66f7
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-03-23 08:41:58 +0900

    chore(global): inital commit

---
+++ src/@types/global.d.ts
@@ -0,0 +1,14 @@
+declare module '*.glb' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gltf' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
