1500991a89c74ab7a852c621308d84ac981f9055
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-03-23 21:34:12 +0900

    fix(nav): style change

--- src/components/nav/nav.style.ts
+++ src/components/nav/nav.style.ts
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 export const NavWrapper = styled.div`
   width: 100vw;
   height: 80px;
-  background-color: #888;
+  background-color: rgba(150, 150, 150, 0.5);
+  position: fixed;
+  z-index: 9999;
 `;
