6c87b838019223f0c5b4fced2ba2e9c95b79a513
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-04-01 21:16:07 +0900

    add:mainScreen
    0401 메인스크린 개발

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,9 +3,11 @@ import Route from './routes.d';
 import NotfoundPage from '$/pages/notfound/notfound';
 import TestPage from '$/pages/test/App';
 import RealTest from '$/pages/test/Test'
+import MainPage from '$/pages/main/main';
 
 const routes: Array<Route> = [
-  { path: '/', component: TestPage },
+  { path: '/', component: MainPage, footer: false },
+  { path: 'main', component: MainPage },
   { path: '/test', component: RealTest },
   { path: '*', component: NotfoundPage },
 ];
