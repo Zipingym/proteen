657f0dcafd8d0914850faabe9374252437927d38
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2023-04-02 16:31:28 +0900

    ex_bulletin view push.
    운동 게시글 페이지 뷰

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,10 +4,12 @@ import NotfoundPage from '$/pages/notfound/notfound';
 import TestPage from '$/pages/test/App';
 import RealTest from '$/pages/test/Test'
 import Ex_register from '$/pages/exercise/register/ex_register';
+import Ex_bulletin from '$/pages/exercise/bulletin/ex_bulletin';
 
 const routes: Array<Route> = [
   { path: '/', component: TestPage },
   { path: '/exercise/register', component: Ex_register, footer: false, nav:false },
+  { path: '/exercise/bulletin', component: Ex_bulletin, footer: false, nav:false },
   { path: '/test', component: RealTest },
   { path: '*', component: NotfoundPage },
 ];
