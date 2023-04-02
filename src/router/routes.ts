b5d22bea728877de9eb705d9498f287dded22e84
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2023-04-02 14:15:56 +0900

    ex_register view push.
    운동분석 - 운동 등록 페이지 뷰

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,9 +3,11 @@ import Route from './routes.d';
 import NotfoundPage from '$/pages/notfound/notfound';
 import TestPage from '$/pages/test/App';
 import RealTest from '$/pages/test/Test'
+import Ex_register from '$/pages/exercise/register/ex_register';
 
 const routes: Array<Route> = [
   { path: '/', component: TestPage },
+  { path: '/exercise/register', component: Ex_register, footer: false, nav:false },
   { path: '/test', component: RealTest },
   { path: '*', component: NotfoundPage },
 ];
