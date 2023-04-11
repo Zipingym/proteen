bc6846257fab9423e3386fa7cf7dce63b31ec51f
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-04-12 08:29:47 +0900

    [FIX] exercise regiser design

--- src/pages/exercise/register/ex_register.tsx
+++ src/pages/exercise/register/ex_register.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import * as S from './ex_register.style';
 import Ex_img from './img/Group 91.png';
 
 const Ex_register = () => {
+  const [type, setType] = useState([
+    'Pull-up',
+    'Squat',
+    'Lunge',
+    'Plank',
+    'Crunch',
+  ]);
   return (
     <S.Body>
       <S.AllContainer>
@@ -14,7 +21,62 @@ const Ex_register = () => {
           <S.Reels_view src={Ex_img} alt="Error" />
 
           <S.Write>
-            <S.Content>
+            <S.InputWrapper height={10}>
+              <S.Label>
+                <h4>제목</h4>
+                <h6>*</h6>
+              </S.Label>
+              <S.InputTitle></S.InputTitle>
+            </S.InputWrapper>
+            <S.InputWrapper height={40}>
+              <S.Label>
+                <h4>운동 내용</h4>
+                <h6>*</h6>
+              </S.Label>
+              <S.InputInfo></S.InputInfo>
+              <S.ChoseBtn>
+                {type.map((type: string) => {
+                  return (
+                    <S.CB>
+                      <pre> {type} </pre>
+                    </S.CB>
+                  );
+                })}
+              </S.ChoseBtn>
+            </S.InputWrapper>
+            <S.InputWrapper height={25}>
+              <S.Label>
+                <h4>운동 기록</h4>
+                <h6>*</h6>
+              </S.Label>
+              <S.Record>
+                <S.rContent>
+                  <S.rTitle>평균 점수</S.rTitle>
+                  <S.rTitle>운동 시간</S.rTitle>
+                  <S.rTitle>소모 칼로리</S.rTitle>
+                </S.rContent>
+
+                <S.rElement>
+                  <S.rElementB>78.2</S.rElementB>
+                  <S.rTime>
+                    <S.rElementB>01</S.rElementB>
+                    <S.rElementS>h</S.rElementS>
+                    <S.rElementB>30</S.rElementB>
+                    <S.rElementS>m</S.rElementS>
+                  </S.rTime>
+
+                  <S.rKcal>
+                    <S.rElementB>150</S.rElementB>
+                    <S.rElementS>kcal</S.rElementS>
+                  </S.rKcal>
+                </S.rElement>
+              </S.Record>
+            </S.InputWrapper>
+            <S.submit>
+              <S.submitU>업로드</S.submitU>
+            </S.submit>
+
+            {/* <S.Content>
               <S.cTitle>제목</S.cTitle>
               <S.cSymbol>*</S.cSymbol>
             </S.Content>
@@ -73,7 +135,7 @@ const Ex_register = () => {
               <S.submit>
                 <S.submitU>업로드</S.submitU>
               </S.submit>
-            </S.Record>
+            </S.Record> */}
           </S.Write>
         </S.MainContainer>
       </S.AllContainer>
