944e20b26e8d8b6a7b7ab8d89c34ea5343813bb8
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-05-02 21:55:55 +0900

    Update ex_register.tsx

--- src/pages/exercise/register/ex_register.tsx
+++ src/pages/exercise/register/ex_register.tsx
@@ -1,9 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import * as S from './ex_register.style';
 import Ex_img from './img/Group 91.png';
+import axios from 'axios';
+import api from '$/api/customAxios';
+interface Post {
+  request: {
+    title: string;
+    body: string;
+    exerciseType: string;
+    score: number;
+    time: string;
+    calorie: number;
+  };
+  file: string;
+}
 
 const Ex_register = () => {
+  const [post, setPost] = useState<Post>({
+    request: {
+      title: '',
+      body: '',
+      exerciseType: 'PULLUP',
+      score: 0,
+      time: '',
+      calorie: 0,
+    },
+    file: '',
+  });
+  api
+    .post('/user/signup', post)
+    .then(console.log)
+    .catch((err) => {
+      console.log(err);
+      // console.log()
+    });
+  useEffect(() => {
+    console.log(post);
+  });
   const [type, setType] = useState([
     'Pull-up',
     'Squat',
@@ -11,6 +45,10 @@ const Ex_register = () => {
     'Plank',
     'Crunch',
   ]);
+  const handleChangerUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setPost({ ...post, [name]: value });
+  };
   return (
     <S.Body>
       <S.AllContainer>
@@ -35,15 +73,25 @@ const Ex_register = () => {
                   <h4>제목</h4>
                   <h6>*</h6>
                 </S.Label>
-                <S.InputTitle></S.InputTitle>
+                <S.InputTitle
+                  name="request.title"
+                  type="text"
+                  placeholder="제목"
+                  onChange={handleChangerUser}
+                ></S.InputTitle>
               </S.InputWrapper>
               <S.InputWrapper height={40}>
                 <S.Label>
                   <h4>운동 내용</h4>
                   <h6>*</h6>
                 </S.Label>
-                <S.InputInfo></S.InputInfo>
-                <S.ChoseBtn>
+                <S.InputInfo
+                  name="body"
+                  type="text"
+                  placeholder="내용"
+                  onChange={handleChangerUser}
+                ></S.InputInfo>
+                <S.ChoseBtn name="exerciseType" onChange={handleChangerUser}>
                   {type.map((type: string) => {
                     return (
                       <S.CB>
@@ -84,67 +132,6 @@ const Ex_register = () => {
               <S.submit>
                 <S.submitU>업로드</S.submitU>
               </S.submit>
-
-              {/* <S.Content>
-              <S.cTitle>제목</S.cTitle>
-              <S.cSymbol>*</S.cSymbol>
-            </S.Content>
-            <S.inputTitle type="text" placeholder="제목을 입력해 주세요." />
-
-            <S.Content>
-              <S.cTitle>운동</S.cTitle>
-              <S.cSymbol>*</S.cSymbol>
-            </S.Content>
-            <S.inputInfo placeholder="운동 내용을 작성해 주세요." />
-
-            <S.ChoseBtn>
-              <S.CB>
-                <pre> Pull-up </pre>
-              </S.CB>
-              <S.CB>
-                <pre> Squat </pre>
-              </S.CB>
-              <S.CB>
-                <pre> Lunge </pre>
-              </S.CB>
-              <S.CB>
-                <pre> Plank </pre>
-              </S.CB>
-              <S.CB>
-                <pre> Crunch </pre>
-              </S.CB>
-            </S.ChoseBtn>
-
-            <S.Content>
-              <S.cTitle>운동 기록</S.cTitle>
-              <S.cSymbol>*</S.cSymbol>
-            </S.Content>
-
-            <S.Record>
-              <S.rContent>
-                <S.rTitle>평균 점수</S.rTitle>
-                <S.rTitle>운동 시간</S.rTitle>
-                <S.rTitle>소모 칼로리</S.rTitle>
-              </S.rContent>
-
-              <S.rElement>
-                <S.rElementB>78.2</S.rElementB>
-                <S.rTime>
-                  <S.rElementB>01</S.rElementB>
-                  <S.rElementS>h</S.rElementS>
-                  <S.rElementB>30</S.rElementB>
-                  <S.rElementS>m</S.rElementS>
-                </S.rTime>
-
-                <S.rKcal>
-                  <S.rElementB>150</S.rElementB>
-                  <S.rElementS>kcal</S.rElementS>
-                </S.rKcal>
-              </S.rElement>
-              <S.submit>
-                <S.submitU>업로드</S.submitU>
-              </S.submit>
-            </S.Record> */}
             </S.Write>
           </S.MainContainer>
         </S.Container>
